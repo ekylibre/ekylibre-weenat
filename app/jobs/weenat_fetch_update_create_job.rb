3eb4e8651370f947bdbea402d27863fbb5e0a7fc
thibaut <tgorioux@ekylibre.com>
2020-06-29 16:48:12 +0200

    take into account of maximum 10days period rules of weenat api in FetchUpdateCreateJob

--- app/jobs/weenat_fetch_update_create_job.rb
+++ app/jobs/weenat_fetch_update_create_job.rb
@@ -5,8 +5,7 @@ class WeenatFetchUpdateCreateJob < ActiveJob::Base
   def perform(last_imported_at)
 
     # compute start and stop in EPOCH timestamp for weenat API
-    started_at = last_imported_at
-    stopped_at = Time.zone.now.to_i
+    time_now = Time.zone.now.to_i
 
     last_sampled_at_list = []
 
@@ -47,44 +46,54 @@ class WeenatFetchUpdateCreateJob < ActiveJob::Base
             last_transmission_at: Time.zone.now
           )
 
-          # Get data for a plot (plot[:id]) and create analyse and items
-          Weenat::WeenatIntegration.last_values(plot[:id], started_at, stopped_at).execute do |c|
-            c.success do |values|
-              values.each do |plot_analysis|
-                reference_number = sensor.euid.to_s + "_" + plot_analysis[0].to_s
-                read_at = Time.at(plot_analysis[0].to_s.to_i)
 
-                analyse = sensor.analyses.find_or_create_by(
-                  reference_number: reference_number,
-                  sampled_at: read_at,
-                  analysed_at: read_at,
-                  retrieval_status: :ok,
-                  nature: :sensor_analysis,
-                  geolocation: geolocation,
-                  sampling_temporal_mode: :period
-                )
-                # Avoid re creations of the same items if analyse exist with items
-                unless analyse.items.any?
-                  # Transcode each item present with transcode_indicators and save it
-                  plot_analysis[1].each do |plot_analysis_item|
-                    transcoded_indicator = transcode_indicators[plot_analysis_item.first]
-                    if transcoded_indicator && plot_analysis_item.last != nil
-                      analyse.read!(transcoded_indicator[:indicator], plot_analysis_item.last.in(transcoded_indicator[:unit]))
+          period = time_now - last_imported_at
+          periods_count = period / 10.days
+          last_period_length = period % 10.days
+
+          # call "periods_count" times 10 days because of Weenat api refuse more than 10 days.
+          (0...periods_count).to_a.reverse.each do |i|
+            # compute start and stop in EPOCH timestamp for weenat API
+            started_at = time_now - last_period_length - (i * 10.days)
+            stopped_at = time_now - ((i - 1) * 10.days) - (i.zero? ? 0 : last_period_length)
+
+            # Get data for a plot (plot[:id]) and create analyse and items
+            Weenat::WeenatIntegration.last_values(plot[:id], started_at, stopped_at).execute do |c|
+              c.success do |values|
+                values.each do |plot_analysis|
+                  reference_number = sensor.euid.to_s + "_" + plot_analysis[0].to_s
+                  read_at = Time.at(plot_analysis[0].to_s.to_i)
+
+                  analyse = sensor.analyses.find_or_create_by(
+                    reference_number: reference_number,
+                    sampled_at: read_at,
+                    analysed_at: read_at,
+                    retrieval_status: :ok,
+                    nature: :sensor_analysis,
+                    geolocation: geolocation,
+                    sampling_temporal_mode: :period
+                  )
+                  # Avoid re creations of the same items if analyse exist with items
+                  unless analyse.items.any?
+                    # Transcode each item present with transcode_indicators and save it
+                    plot_analysis[1].each do |plot_analysis_item|
+                      transcoded_indicator = transcode_indicators[plot_analysis_item.first]
+                      if transcoded_indicator && plot_analysis_item.last != nil
+                        analyse.read!(transcoded_indicator[:indicator], plot_analysis_item.last.in(transcoded_indicator[:unit]))
+                      end
                     end
                   end
-                end
 
-              end
-              if values.any?
-                last_sampled_at_list << values.max_by { |k, _v| k }.first.to_s.to_i
+                end
+                if values.any?
+                  last_sampled_at_list << values.max_by { |k, _v| k }.first.to_s.to_i
+                end
               end
             end
           end
         end
       end
     end
-
     Preference.set!('weenat_import', last_sampled_at_list.min, 'integer')
   end
-
 end
